b684e11f49c8b0aa478ee5d39f39ea2a3476ca0a
eatnows <gagyasindev@gmail.com>
2021-12-18 00:33:00 +0900

    feat: 쓰레드를 활용하여 캐릭터 움직이기

--- src/test/ex04/Player.java
+++ src/test/ex04/Player.java
@@ -1,9 +1,14 @@
-package test.ex03;
+package test.ex04;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.swing.*;
 
 // class Player -> new 가능, 게임에 존재할 수 있음. (추상메서드를 가질 수 없다,)
-public class Player extends JLabel implements Moveable{
+@Getter
+@Setter
+public class Player extends JLabel implements Moveable {
 
     // 위치 상태
     private int x;
@@ -44,16 +49,38 @@ public class Player extends JLabel implements Moveable{
     // 이벤트 핸들러
     @Override
     public void left() {
-        setIcon(playerL);
-        x -= 10;
-        setLocation(x, y);
+        System.out.println("left");
+        left = true;
+        new Thread(() -> {
+            while (left) {
+                setIcon(playerL);
+                x -= 1;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10); // 0.01 초
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
     public void right() {
-        setIcon(playerR);
-        x += 10;
-        setLocation(x, y);
+        System.out.println("right");
+        right = true;
+        new Thread(() -> {
+            while (right) {
+                setIcon(playerR);
+                x += 1;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10); // 0.01 초
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
